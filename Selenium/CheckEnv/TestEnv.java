35d323a85a9b3cfcc8de5362f798353485a63b08
Mai Anh Trung <anh.maitrung@gmail.com>
2020-09-12 21:46:56 +0700

    Chech environment

---
+++ Selenium/CheckEnv/TestEnv.java
@@ -0,0 +1,26 @@
+package CheckEnv;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class TestEnv {
+	WebDriver driver;
+	
+  @Test
+  public void TC_01() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new FirefoxDriver();
+	  driver.get("http:/facebook.com");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  driver.quit();
+  }
+
+}
