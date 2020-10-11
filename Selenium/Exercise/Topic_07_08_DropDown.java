201ad85163305a25e89519a49414b1dc7034dfa0
Mai Anh Trung <anh.maitrung@gmail.com>
2020-10-11 18:52:45 +0700

    update topic 16

--- Selenium/Exercise/Topic_07_08_DropDown.java
+++ Selenium/Exercise/Topic_07_08_DropDown.java
@@ -4,7 +4,6 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -13,7 +12,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.server.handler.FindElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
